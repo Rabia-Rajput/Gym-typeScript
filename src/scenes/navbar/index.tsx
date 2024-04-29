ab28ed847446a0ac15aed77ea169768b3189adee
Rabia-Rajput <rajput.rabia@outlook.com>
2024-04-29 22:46:46 +0200

    updated file

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -3,6 +3,8 @@ import { Bars3Icon , XMarkIcon } from "@heroicons/react/16/solid";
 import Logo from '@/assets/Logo.png';
 import Link from "./Link";
 import { SelectedPage } from "@/shared/types";
+import useMediaQuery from "@/hooks/useMediaQuery";
+import ActionButton from "@/shared/ActionButton";
 
 
 
@@ -17,14 +19,15 @@ const Navbar = ({
 
 }: Props) => {
     const flexbetween ='flex items-center justify-between';
+    const isAboveMediumScreens = useMediaQuery('(min-width:1060px)');
+    const [isMenuToggled, setIsMenuToggled]=useState(false);
   return (
     <nav>
         <div className={`${flexbetween}fixed top-0 z-30 w-full py-6`} >
             <div className={`${flexbetween} mx-auto w-5/6`}>
                 <div  className={`${flexbetween} w-full gap-16`}>
                     <img alt="logo" src={Logo}/>
-
-                    <div  className={`${flexbetween} w-full`}>
+                    {isAboveMediumScreens ? (<div  className={`${flexbetween} w-full`}>
                         <div className={`${flexbetween} gap-8 text-sm`}>
                              <Link page = "Home" selectedPage={selectedPage}
                              setSelectedPage={setSelectedPage}
@@ -37,17 +40,20 @@ const Navbar = ({
                              <Link page ="Contact Us"
                               selectedPage={selectedPage}
                               setSelectedPage={setSelectedPage}/>
-                           
-
-                        </div>
+                        </div> 
                             <div className={`${flexbetween} gap-8 `}>
                                 <p>Sign In</p>
-                                <button>Become a Member</button>
-                                
-
-                        </div>
-
-                    </div>
+                                <ActionButton setSelectedPage={setSelectedPage}>Become a Member</ActionButton>
+                        </div> 
+                        {/* represent desktop screen */}
+                    </div> )
+                    // represent mobile screen
+                     : (
+                        <button className="rounded-full bg-secondary-500 p-2" 
+                        onClick={()=>setIsMenuToggled(!isMenuToggled)}> 
+                        <Bars3Icon className="h-6 w-6 text-white"/>
+                        </button>
+                    )}
 
                 </div>
 
