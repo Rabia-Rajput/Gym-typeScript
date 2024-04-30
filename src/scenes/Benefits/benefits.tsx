5b520a8a94753ccc4ac686da6040f93ac37d28de
Rabia-Rajput <rajput.rabia@outlook.com>
2024-04-30 18:00:24 +0200

    updated file

--- src/scenes/Benefits/benefits.tsx
+++ src/scenes/Benefits/benefits.tsx
@@ -112,7 +112,16 @@ const Benefits = ({ setSelectedPage }: Props) => {
             </div>
 
             {/* DESCRIPT */}
-            <motion.div >
+            <motion.div
+              initial="hidden"
+              whileInView="visible"
+              viewport={{ once: true, amount: 0.5 }}
+              transition={{ delay: 0.2, duration: 0.5 }}
+              variants={{
+                hidden: { opacity: 0, x: 50 },
+                visible: { opacity: 1, x: 0 },
+              }}
+            >
               <p className="my-5">
                 Nascetur aenean massa auctor tincidunt. Iaculis potenti amet
                 egestas ultrices consectetur adipiscing ultricies enim. Pulvinar
