33d70cc1bd945d241cd89577df7c0aeec2f29e97
Rabia-Rajput <rajput.rabia@outlook.com>
2024-04-30 14:10:45 +0200

    updated file

--- src/scenes/navbar/index.tsx
+++ src/scenes/navbar/index.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Bars3Icon , XMarkIcon } from "@heroicons/react/16/solid";
+import { Bars3Icon , XMarkIcon } from "@heroicons/react/24/solid";
 import Logo from '@/assets/Logo.png';
 import Link from "./Link";
 import { SelectedPage } from "@/shared/types";
@@ -14,14 +14,10 @@ type Props = {
     setSelectedPage:(value:SelectedPage)=>void;
 };
 
-const Navbar = ({
-    isTopOfPage,
-    selectedPage,
-    setSelectedPage,
-}: Props) => {
+const Navbar = ({ isTopOfPage, selectedPage, setSelectedPage}: Props) => {
     const flexbetween ='flex items-center justify-between';
-    const isAboveMediumScreens = useMediaQuery('(min-width:1060px)');
     const [isMenuToggled, setIsMenuToggled]=useState<boolean>(false);
+    const isAboveMediumScreens = useMediaQuery('(min-width:1060px)');
     const navbarBackground = isTopOfPage ? "" : 'bg-primary-100 drop-shadow';
   return (
     <nav>
@@ -30,7 +26,7 @@ const Navbar = ({
             <div className={`${flexbetween} mx-auto w-5/6`}>
                 <div  className={`${flexbetween} w-full gap-16`}>
                     <img alt="logo" src={Logo}/>
-                    {isAboveMediumScreens ? (<div  className={`${flexbetween} w-full`}>
+                    {isAboveMediumScreens ? ( <div  className={`${flexbetween} w-full`}>
                         <div className={`${flexbetween} gap-8 text-sm`}>
                              <Link page = "Home" selectedPage={selectedPage}
                              setSelectedPage={setSelectedPage}
@@ -49,9 +45,9 @@ const Navbar = ({
                                 <ActionButton setSelectedPage={setSelectedPage}>Become a Member</ActionButton>
                         </div> 
                         
-                    </div> )
+                    </div> ): (
                     // represent mobile screen
-                     : (
+                     
                         <button className="rounded-full bg-secondary-500 p-2" 
                         onClick={()=>setIsMenuToggled(!isMenuToggled)}> 
                         <Bars3Icon className="h-6 w-6 text-white"/>
