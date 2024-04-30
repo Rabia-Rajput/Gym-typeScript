f823bb5e6e23b75bdbf50692e0e857ff540ec2f4
Rabia-Rajput <rajput.rabia@outlook.com>
2024-04-30 14:10:35 +0200

    updated file

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,26 @@
 import Navbar from '@/scenes/navbar';
 import { useEffect, useState } from 'react';
 import { SelectedPage } from '@/shared/types';
+import Home from './scenes/Home/Home';
+
 function App() {
-  const [selectedPage, setSelectedPage]=useState<SelectedPage>(SelectedPage.Home);
-  const [isTopOfPage , setIsTopOfPage] =useState<boolean>(true);
-  useEffect(()=>{
-    const handleScroll =()=>{
-      if(window.scrollY === 0){
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(
+    SelectedPage.Home
+  );
+  const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) {
         setIsTopOfPage(true);
         setSelectedPage(SelectedPage.Home);
-      } 
-      else { setIsTopOfPage(false);
       }
-      window.addEventListener('scroll', handleScroll);
-      return () => window.removeEventListener('scroll', handleScroll);
-    }
+      if (window.scrollY !== 0) setIsTopOfPage(false);
+    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
   }, []);
+
   return <div className="app bg-gray-20 ">
     <Navbar isTopOfPage={isTopOfPage} 
     selectedPage={selectedPage}
