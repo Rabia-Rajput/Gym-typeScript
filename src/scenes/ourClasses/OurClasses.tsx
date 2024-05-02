6724d105e1064930f337868afb9a38ac7808814d
Rabia-Rajput <rajput.rabia@outlook.com>
2024-05-02 18:28:32 +0200

    updated file

--- src/scenes/ourClasses/OurClasses.tsx
+++ src/scenes/ourClasses/OurClasses.tsx
@@ -1,60 +1,57 @@
-import { ClassType, SelectedPage } from "@/shared/types";
-import image1 from '@/assets/image1.png';
-import image2 from '@/assets/image2.png';
-import image3 from '@/assets/image3.png';
-import image4 from '@/assets/image4.png';
-import image5 from '@/assets/image5.png';
-import image6 from '@/assets/image6.png';
+import { SelectedPage, ClassType } from "@/shared/types";
+import image1 from "@/assets/image1.png";
+import image2 from "@/assets/image2.png";
+import image3 from "@/assets/image3.png";
+import image4 from "@/assets/image4.png";
+import image5 from "@/assets/image5.png";
+import image6 from "@/assets/image6.png";
 import { motion } from "framer-motion";
 import HText from "@/shared/HText";
 import Class from "./Class";
+
 const classes: Array<ClassType> = [
-    {
-      name: "Weight Training Classes",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-      image: image1,
-    },
-    {
-      name: "Yoga Classes",
-      image: image2,
-    },
-    {
-      name: "Ab Core Classes",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-      image: image3,
-    },
-    {
-      name: "Adventure Classes",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-      image: image4,
-    },
-    {
-      name: "Fitness Classes",
-      image: image5,
-    },
-    {
-      name: "Training Classes",
-      description:
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
-      image: image6,
-    },
-  ];
-  
+  {
+    name: "Weight Training Classes",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    image: image1,
+  },
+  {
+    name: "Yoga Classes",
+    image: image2,
+  },
+  {
+    name: "Ab Core Classes",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    image: image3,
+  },
+  {
+    name: "Adventure Classes",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    image: image4,
+  },
+  {
+    name: "Fitness Classes",
+    image: image5,
+  },
+  {
+    name: "Training Classes",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+    image: image6,
+  },
+];
 
 type Props = {
-    setSelectedPage: (value:SelectedPage)=>void;
+  setSelectedPage: (value: SelectedPage) => void;
 };
 
-const OurClasses = ({setSelectedPage}: Props) => {
+const OurClasses = ({ setSelectedPage }: Props) => {
   return (
-     <section id="ourclasses" className="w-full bg-primary-100 py-40 ">
-    
-    <motion.div
-        onViewportEnter={() => setSelectedPage(SelectedPage.OurClass)}
-      >
+    <section id="ourclasses" className="w-full bg-primary-100 py-40">
+      <motion.div onViewportEnter={() => setSelectedPage(SelectedPage.OurClass)}>
         <motion.div
           className="mx-auto w-5/6"
           initial="hidden"
@@ -88,7 +85,7 @@ const OurClasses = ({setSelectedPage}: Props) => {
             ))}
           </ul>
         </div>
-        </motion.div>
+      </motion.div>
     </section>
   );
 };
