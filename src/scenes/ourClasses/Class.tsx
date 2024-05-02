be7046c5bb50efd133bbcb81b82bf27c3754bc0d
Rabia-Rajput <rajput.rabia@outlook.com>
2024-05-02 15:07:08 +0200

    updated file

---
+++ src/scenes/ourClasses/Class.tsx
@@ -0,0 +1,18 @@
+
+type Props = {
+
+  name: string;
+   description?:string;
+   image:string; 
+}
+
+const Class = ({ name , description , image }: Props) => {
+  return (
+   <li className=" relative mx-5 in-line-block h-[380px] w-[450px]">
+  
+     
+   </li>
+  )
+}
+
+export default Class;
