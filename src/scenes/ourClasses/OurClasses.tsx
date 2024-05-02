8cd03ab203b8999239026af424d68b6c297353f2
Rabia-Rajput <rajput.rabia@outlook.com>
2024-05-02 14:40:09 +0200

    updated file

--- src/scenes/ourClasses/OurClasses.tsx
+++ src/scenes/ourClasses/OurClasses.tsx
@@ -5,6 +5,8 @@ import image3 from '@/assets/image3.png';
 import image4 from '@/assets/image4.png';
 import image5 from '@/assets/image5.png';
 import image6 from '@/assets/image6.png';
+import { motion } from "framer-motion";
+import HText from "@/shared/HText";
 
 type Props = {
     setSelectedPage: (value:SelectedPage)=>void;
@@ -12,8 +14,29 @@ type Props = {
 
 const OurClasses = ({setSelectedPage}: Props) => {
   return (
-     <section id="ourclasses" className="w-full bg-primary-100 py-40 "
-    >OurClasses</section>
+     <section id="ourclasses" className="w-full bg-primary-100 py-40 ">OurClasses
+    
+    <motion.div
+        onViewportEnter={() => setSelectedPage(SelectedPage.OurClass)}
+      >
+        <motion.div
+          className="mx-auto w-5/6"
+          initial="hidden"
+          whileInView="visible"
+          viewport={{ once: true, amount: 0.5 }}
+          transition={{ duration: 0.5 }}
+          variants={{
+            hidden: { opacity: 0, x: -50 },
+            visible: { opacity: 1, x: 0 },
+          }}
+        >
+          <div className="md:w-3/5">
+            <HText>OUR CLASSES</HText>
+          
+          </div>
+        </motion.div>
+        </motion.div>
+    </section>
   );
 };
 
